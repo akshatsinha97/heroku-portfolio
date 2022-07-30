803e83e0e8f88570bae0bd926ebccdbee79b42f3
Akshat Sinha <akshatsinha97@gmail.com>
2022-07-30 20:18:51 +0530

    changed back urls.py

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('home.urls')),
+    path('', include('home.urls')),
     path('akshat/', include('home.urls'))
     
 ]
